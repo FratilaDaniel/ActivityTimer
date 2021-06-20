75cff360adb2cfd84eb724568e95504580686b5e
FratilaDaniel <danfratila1997@gmail.com>
2021-06-20 18:14:48 +0300

    add navbar with hover animations

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+
+function Navbar(props){
+    return (
+        <div className="navbar-container">
+            <div className="logo-container">
+                LOGO
+            </div>
+
+            <div className="menu-container">
+                <ul>
+                    <li><a href="/#">Contact</a></li>
+                    <li><a href="/#">Help</a></li>
+                    <li><a href="/#">Settings</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
