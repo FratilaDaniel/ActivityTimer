14610b389ae2569499cbe025c593849ebcb48f39
FratilaDaniel <danfratila1997@gmail.com>
2021-06-29 11:20:46 +0300

    add footer with github and linkedin links, also credits

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
-
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 function Footer(){
     return (
         <div className="footer-container">
-            This is the footer
+            <div className="links-container">
+                <a href="https://www.linkedin.com/in/daniel-fratila-369910149/"><GitHubIcon/></a>
+                <a href="https://github.com/FratilaDaniel"><LinkedInIcon/></a>
+            </div>
+            <p className="credits">©{new Date().getUTCFullYear()} Daniel Fratila</p>
         </div>
     );
 }
