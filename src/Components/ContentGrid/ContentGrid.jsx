5cd1fe536790455f3cdc244417f7ba03fc90e7e2
FratilaDaniel <danfratila1997@gmail.com>
2021-08-15 12:33:30 +0300

    improve performance through lazy loading

--- src/Components/ContentGrid/ContentGrid.jsx
+++ src/Components/ContentGrid/ContentGrid.jsx
@@ -1,18 +1,23 @@
+import React, {Suspense} from 'react';
 import SmartActivityInput from "./views/ActivityInput/SmartActivityInput";
-import SmartSchedule from "./views/Table/SmartSchedule";
-import SmartStatistics from "./views/Table/SmartStatistics";
-import SmartChart from "./views/Chart/SmartChart";
+const SmartSchedule = React.lazy(() => import("./views/Table/SmartSchedule"));
+const SmartChart = React.lazy(() => import("./views/Chart/SmartChart"));
+const SmartStatistics = React.lazy(() => import("./views/Table/SmartStatistics"));
 
 function ContentGrid() {
     return (
         <div className="content-container">
             <div className="top-flex">
                 <SmartActivityInput/>
-                <SmartChart/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <SmartChart/>
+                </Suspense>
             </div>
             <div className="bot-flex">
-                <SmartSchedule/>
-                <SmartStatistics/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <SmartSchedule/>
+                    <SmartStatistics/>
+                </Suspense>
             </div>   
         </div>
     );
